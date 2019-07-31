71b6e63465890f35febfea0508057e7bf51f370f
Erick Rincones <erick.rincones@ciens.ucv.ve>
2019-07-30 23:58:35 -0400

    Add the Camera class

---
+++ src/scene/camera.cpp
@@ -0,0 +1,362 @@
+#include "camera.hpp"
+
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/trigonometric.hpp>
+
+
+// Static attributes
+
+// Boost flag
+bool Camera::boosted = false;
+
+// Normal speed
+float Camera::speed = 0.5F;
+
+// Boosted speed
+float Camera::boosted_speed = 1.0F;
+
+
+// Mouse sensibility
+float Camera::sensibility = 15.0F;
+
+
+// Zoom factor
+float Camera::zoom_factor = 1.0625F;
+
+
+// Private methods
+
+// updateViewMatrix
+void Camera::updateViewMatrix() {
+    view_mat = glm::lookAt(position, position + front, glm::cross(right, front));
+}
+
+// Update projection matrices
+void Camera::updateProjectionMatrices() {
+    // Auxiliar values
+    const float aspect = (float)width / (float)height;
+    const float ratio = glm::atan(fov / 2.0F);
+    const float distance = glm::length(position);
+
+    // Orthogonal limits
+    const float y = ratio * distance;
+    const float x = y * aspect;
+
+    // Projection matrices
+    orthogonal_mat = glm::ortho(-x, x, -y, y, near, far);
+    perspective_mat = glm::perspective(fov, aspect, near, far);
+}
+
+
+// Constructor
+
+// Camera constructor
+Camera::Camera(const int &width, const int &height, const bool &orthogonal) :
+    orthogonal(orthogonal),
+    width(width),
+    height(height) {
+    // Load default values
+    reset();
+}
+
+
+// Getters
+
+// Get the orthogonal projection status
+bool Camera::isOrthogonal() const {
+    return orthogonal;
+}
+
+
+// Get position
+glm::vec3 Camera::getPosition() const {
+    return position;
+}
+
+// Get look direction
+glm::vec3 Camera::getDirection() const {
+    return front;
+}
+
+// Get look angles
+glm::vec3 Camera::getRotation() const {
+    return glm::degrees(glm::vec3(glm::atan(front.z, front.x), glm::asin(front.y), glm::asin(up.x)));
+}
+
+// Get up vector
+glm::vec3 Camera::getUp() const {
+    return up;
+}
+
+
+// Get field of view
+float Camera::getFOV() const {
+    return glm::degrees(fov);
+}
+
+// Get resolution
+glm::vec2 Camera::getResolution() const {
+    return glm::vec2(width, height);
+}
+
+// Get clipping
+glm::vec2 Camera::getClipping() const {
+    return glm::vec2(near, far);
+}
+
+
+// Get the view matrix
+glm::mat4 Camera::getViewMatrix() const {
+    return view_mat;
+}
+
+// Get the orthogonal projection matrix
+glm::mat4 Camera::getOrthogonalMatrix() const {
+    return orthogonal_mat;
+}
+
+// Get the perspective projection matrix
+glm::mat4 Camera::getPerspectiveMatrix() const {
+    return perspective_mat;
+}
+
+// Get the current projection matrix
+glm::mat4 Camera::getProjectionMatrix() const {
+    return orthogonal ? orthogonal_mat : perspective_mat;
+}
+
+
+// Setters
+
+// Set the orthogonal projection status
+void Camera::isOrthogonal(const bool &status) {
+    orthogonal = status;
+}
+
+
+// Set position
+void Camera::setPosition(const glm::vec3 &new_position) {
+    position = new_position;
+}
+
+// Set look direction
+void Camera::setDirection(const glm::vec3 &new_direction) {
+    front = glm::normalize(new_direction);
+    right = glm::cross(front, up);
+    updateViewMatrix();
+}
+
+// Set look angles
+void Camera::setRotation(const glm::vec3 &angle) {
+    // Angles to radian
+    glm::vec3 radian = glm::radians(glm::vec3(angle.x, glm::clamp(angle.y, -89.0F, 89.0F), angle.z));
+
+    // Update the direction vector
+    front.x = glm::cos(radian.y) * glm::cos(radian.x);
+    front.y = glm::sin(radian.y);
+    front.z = glm::cos(radian.y) * glm::sin(radian.x);
+    front = glm::normalize(front);
+
+    // Update up world vector
+    up.x = glm::sin(radian.z);
+    up.y = glm::cos(radian.z);
+    up = glm::normalize(up);
+
+    // Update right vector
+    right = glm::normalize(glm::cross(front, up));
+
+    // Update view matrix
+    updateViewMatrix();
+}
+
+// Set up vector
+void Camera::setUp(const glm::vec3 &direction) {
+    up = glm::normalize(direction);
+    right = glm::normalize(glm::cross(front, up));
+    updateViewMatrix();
+}
+
+
+// Set field of view
+void Camera::setFOV(const float &new_fov) {
+    fov = glm::radians(new_fov);
+    updateProjectionMatrices();        
+}
+
+// Set resolution
+void Camera::setResolution(const glm::vec2 &resolution) {
+    // Set the resolution values
+    width = static_cast<int>(resolution.x);
+    height = static_cast<int>(resolution.y);
+
+    // Prevent divisions by zero
+    if (height == 0) {
+        height = 1;
+    }
+
+    // Update matrices
+    updateProjectionMatrices();
+}
+
+// Set clipping
+void Camera::setClipping(const glm::vec2 &clipping) {
+    near = clipping.x;
+    far = clipping.y;
+    updateProjectionMatrices();
+}
+
+
+// Methods
+
+// Reset to default values
+void Camera::reset() {
+    // View default values
+    position = glm::vec3(0.0F);
+    front    = glm::vec3(0.0F, 0.0F, -1.0F);
+    right    = glm::vec3(1.0F, 0.0F,  0.0F);
+    up       = glm::vec3(0.0F, 1.0F,  0.0F);
+
+    // Projection default values
+    fov  = glm::radians(30.0F);
+    near = 0.01F;
+    far  = 10.0F;
+
+    // Rotation default values
+    yaw   = -90.0F;
+    pitch = 0.0F;
+
+    // Update matrices
+    updateViewMatrix();
+    updateProjectionMatrices();
+}
+
+
+// Bind the camera
+void Camera::bind(GLSLProgram *const program) const {
+    // Check the program
+    if ((program == nullptr) || (!program->isValid())) {
+        return;
+    }
+
+    // Use the program
+    program->use();
+
+    // Set uniforms
+    program->setUniform("up_dir",         up);
+    program->setUniform("view_dir",       front);
+    program->setUniform("view_pos",       position);
+    program->setUniform("view_mat",       view_mat);
+    program->setUniform("projection_mat", orthogonal ? orthogonal_mat : perspective_mat);
+}
+
+
+// Travell the camera
+void Camera::travell(const Camera::Movement &direction, const double &time) {
+    // Calculate distance
+    float distance = (Camera::boosted ? Camera::boosted_speed : Camera::speed) * (float)time;
+
+    // Move camera
+    switch (direction) {
+        case RIGHT: position += right * distance; break;
+        case LEFT:  position -= right * distance; break;
+        case UP:    position += up * distance; break;
+        case DOWN:  position -= up * distance; break;
+        case FRONT: position += glm::normalize(glm::cross(up, right)) * distance; break;
+        case BACK:  position -= glm::normalize(glm::cross(up, right)) * distance; break;
+    }
+
+    // Update the view matrix
+    updateViewMatrix();
+}
+
+// Translate the camera
+void Camera::translate(const glm::vec3 &delta) {
+    position += delta;
+    updateViewMatrix();
+}
+
+// Apply zoom
+void Camera::zoom(const double &direction) {
+    fov = direction > 0 ? fov / Camera::zoom_factor : fov * Camera::zoom_factor;
+    updateProjectionMatrices();
+}
+
+
+// Rotate the camera
+void Camera::rotate(const glm::vec2 &delta) {
+    // Calculate angles
+    yaw += delta.x * Camera::sensibility;
+    pitch = glm::clamp(pitch + delta.y * Camera::sensibility, -89.0F, 89.0F);
+
+    // Update look vector
+    front.x = glm::cos(glm::radians(pitch)) * glm::cos(glm::radians(yaw));
+    front.y = glm::sin(glm::radians(pitch));
+    front.z = glm::cos(glm::radians(pitch)) * glm::sin(glm::radians(yaw));
+    front  = glm::normalize(front);
+
+    // Update right vector
+    right = glm::normalize(glm::cross(front, up));
+
+    // Update view matrix
+    updateViewMatrix();
+}
+
+
+// Static getters
+
+// Get the boost status
+bool Camera::isBoosted() {
+    return Camera::boosted;
+}
+
+// Get the normal speed
+float Camera::getSpeed() {
+    return Camera::speed;
+}
+
+// Get the boosted speed
+float Camera::getBoostedSpeed() {
+    return Camera::boosted_speed;
+}
+
+
+// Get mouse sensibility
+float Camera::getSensibility() {
+    return Camera::sensibility;
+}
+
+
+// Get the zoom factor
+float Camera::getZoomFactor() {
+    return Camera::zoom_factor;
+}
+
+
+// Static setters
+
+// Set the boost status
+void Camera::setBoosted(const bool &status) {
+    Camera::boosted = status;
+}
+
+// Set the normal speed
+void Camera::setSpeed(const float &speed) {
+    Camera::speed = speed;
+}
+
+// Set the boosted speed
+void Camera::setBoostedSpeed(const float &speed) {
+    Camera::boosted_speed = speed;
+}
+
+
+// Set the mouse sensibility
+void Camera::setSensibility(const float &sesibility) {
+    Camera::sensibility = sensibility;
+}
+
+
+// Set the zoom factor
+void Camera::setZoomFactor(const float &factor) {
+    Camera::zoom_factor = factor;
+}
---
+++ src/scene/camera.hpp
@@ -0,0 +1,265 @@
+#ifndef __CAMERA_HPP_
+#define __CAMERA_HPP_
+
+#include "../scene/glslprogram.hpp"
+
+#include <glm/mat4x4.hpp>
+#include <glm/vec3.hpp>
+
+
+class Camera {
+    private:
+        // Attributes
+
+        /** Orthogonal projection flag */
+        bool orthogonal;
+
+
+        /** Position */
+        glm::vec3 position;
+
+        /** View direction */
+        glm::vec3 front;
+
+        /** Right vector */
+        glm::vec3 right;
+
+        /** World up */
+        glm::vec3 up;
+
+
+        /** Resolution with */
+        int width;
+
+        /** Resolution height */
+        int height;
+
+        /** Field of view */
+        float fov;
+
+        /** Plane z near */
+        float near;
+
+        /** Plane z far */
+        float far;
+
+
+        /** View matrix */
+        glm::mat4 view_mat;
+
+        /** Orthogonal projection matrix */
+        glm::mat4 orthogonal_mat;
+
+        /** Perspective projection matrix */
+        glm::mat4 perspective_mat;
+
+
+        /** Rotation  pitch */
+        float pitch;
+
+        /** Rotation yaw */
+        float yaw;
+
+
+        // Constructors
+
+        /** Delete the default constructor */
+        Camera() = delete;
+
+
+        // Methods
+
+        /** updateViewMatrix */
+        void updateViewMatrix();
+
+        /** Update projection matrices */
+        void updateProjectionMatrices();
+
+
+        // Static attributes
+
+        /** Boost flag */
+        static bool boosted;
+
+        /** Normal speed */
+        static float speed;
+
+        /** Boosted speed */
+        static float boosted_speed;
+
+
+        /** Mouse sensibility */
+        static float sensibility;
+
+
+        /** Zoom factor */
+        static float zoom_factor;
+
+
+    public:
+        // Enumerations
+
+        /** Movement in the front-right plane */
+        enum Movement {
+            /** Right */
+            RIGHT,
+
+            /** Left */
+            LEFT,
+
+            /** Up */
+            UP,
+
+            /** Down */
+            DOWN,
+
+            /** Front */
+            FRONT,
+
+            /** Back */
+            BACK
+        };
+
+
+        // Constructor
+
+        /** Camera constructor */
+        Camera(const int &width, const int &height, const bool &orthogonal = false);
+
+
+        // Getters
+
+        /** Get the orthogonal projection status */
+        bool isOrthogonal() const;
+
+
+        /** Get position */
+        glm::vec3 getPosition() const;
+
+        /** Get look direction */
+        glm::vec3 getDirection() const;
+
+        /** Get look angles */
+        glm::vec3 getRotation() const;
+
+        /** Get up vector */
+        glm::vec3 getUp() const;
+
+
+        /** Get field of view */
+        float getFOV() const;
+
+        /** Get resolution */
+        glm::vec2 getResolution() const;
+
+        /** Get clipping */
+        glm::vec2 getClipping() const;
+
+
+        /** Get the view matrix */
+        glm::mat4 getViewMatrix() const;
+
+        /** Get the orthogonal projection matrix */
+        glm::mat4 getOrthogonalMatrix() const;
+
+        /** Get the perspective projection matrix */
+        glm::mat4 getPerspectiveMatrix() const;
+
+        /** Get the current projection matrix */
+        glm::mat4 getProjectionMatrix() const;
+
+
+        // Setters
+
+        /** Set the orthogonal projection status */
+        void isOrthogonal(const bool &status);
+
+
+        /** Set position */
+        void setPosition(const glm::vec3 &new_position);
+
+        /** Set look direction */
+        void setDirection(const glm::vec3 &direction);
+
+        /** Set look angles */
+        void setRotation(const glm::vec3 &angle);
+
+        /** Set up vector */
+        void setUp(const glm::vec3 &direction);
+
+
+        /** Set field of view */
+        void setFOV(const float &new_fov);
+
+        /** Set resolution */
+        void setResolution(const glm::vec2 &resolution);
+
+        /** Set clipping */
+        void setClipping(const glm::vec2 &clipping);
+
+
+        // Methods
+
+        /** Reset to default values */
+        void reset();
+
+
+        /** Bind the camera */
+        void bind(GLSLProgram *const program) const;
+
+
+        /** Travell the camera */
+        void travell(const Camera::Movement &direction, const double &time = 1.0 / 30.0);
+
+        /** Translate the camera */
+        void translate(const glm::vec3 &delta);
+
+        /** Apply zoom */
+        void zoom(const double &direction);
+
+
+        /** Rotate the camera */
+        void rotate(const glm::vec2 &delta);
+
+
+        // Static getters
+
+        /** Get the boost status */
+        static bool isBoosted();
+
+        /** Get the normal speed */
+        static float getSpeed();
+
+        /** Get the boosted speed */
+        static float getBoostedSpeed();
+
+
+        /** Get mouse sensibility */
+        static float getSensibility();
+
+
+        /** Get the zoom factor */
+        static float getZoomFactor();
+
+
+        // Static setters
+
+        /** Set the boost status */
+        static void setBoosted(const bool &status);
+
+        /** Set the normal speed */
+        static void setSpeed(const float &speed);
+
+        /** Set the boosted speed */
+        static void setBoostedSpeed(const float &speed);
+
+
+        /** Set the mouse sensibility */
+        static void setSensibility(const float &sesibility);
+
+
+        /** Set the zoom factor */
+        static void setZoomFactor(const float &factor);
+};
+
+#endif // __CAMERA_HPP_
